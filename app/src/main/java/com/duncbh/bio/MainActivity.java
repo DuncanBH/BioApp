159e1a5733c86835c3822e0a63efbb94f2670420
DuncanBH <2031593@champlaincollege.qc.ca>
2022-02-11 22:07:45 -0500

    Functional build with databinding

--- app/src/main/java/com/duncbh/bio/MainActivity.java
+++ app/src/main/java/com/duncbh/bio/MainActivity.java
@@ -1,6 +1,7 @@
 package com.duncbh.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -9,23 +10,30 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.duncbh.bio.data.Bio;
+import com.duncbh.bio.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private Bio bio = new Bio();
     private EditText enterHobbies;
     private TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        enterHobbies = findViewById(R.id.enter_hobbies);
-        hobbies = findViewById(R.id.hobbies_text);
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        bio.setName("John Doe");
+        binding.setBio(bio);
+        binding.doneButton.setOnClickListener(view -> addHobbies(view));
     }
 
 
     public void addHobbies(View view) {
-        hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
-        hobbies.setVisibility(View.VISIBLE);
+        bio.setHobbies(String.format("Hobbies: %s", binding.enterHobbies.getText().toString().trim()));
+        binding.invalidateAll();
+        binding.hobbiesText.setVisibility(View.VISIBLE);
 
         //hide keyboard
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
---
+++ app/src/main/java/com/duncbh/bio/data/Bio.java
@@ -0,0 +1,22 @@
+package com.duncbh.bio.data;
+
+public class Bio {
+    private String name;
+    private String hobbies;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+}
